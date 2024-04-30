c7bc60cd996d668ff7f9889f254b2735c524caf0
Mian-Zaid <zaid7296@gmail.com>
2024-04-30 17:55:24 +0500

    separated the sign in option to other button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { ConnectWallet, useSDK } from "@thirdweb-dev/react";
-import { useUser } from "@thirdweb-dev/react";
+import { useUser, useAddress, useLogin } from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
 import { NextPage } from "next";
 import { useState } from "react";
@@ -7,9 +7,11 @@ import { useState } from "react";
 
 const Home: NextPage = () => {
   const { user } = useUser();
+  const address = useAddress()
+  const { login } = useLogin()
 
   const [signature, setSignature] = useState('N/A')
-  const [address, setAddress] = useState('N/A')
+  const [addres, setAddres] = useState('N/A')
 
   const message = 'Please sign me';
 
@@ -32,7 +34,7 @@ const Home: NextPage = () => {
       throw new Error('No Address!');
     }
 
-    setAddress(add)
+    setAddres(add)
   }
 
 
@@ -46,15 +48,22 @@ const Home: NextPage = () => {
         <div className={styles.connect}>
           <ConnectWallet
             auth={{
-              loginOptional: false,
+              loginOptional: true,
             }} />
         </div>
-        {user && <p>You are signed in as {user.address}</p>}
+        {address && <p>Address is {address}</p>}
+
+        <button onClick={() => { login() }}>
+          Sign in with Etheruem
+        </button>
+
+        {user && <p>You are Logged in as user: {user.address}</p>}
+
         {/* <button onClick={signMessage}>Sign message</button>
         <p>Signature: {signature}</p>
         <br />
         <button onClick={recoverAddress}>Recover Wallet Address</button>
-        <p>Wallet Address: {address}</p> */}
+        <p>Wallet Address: {addres}</p> */}
       </div>
 
     </div>
