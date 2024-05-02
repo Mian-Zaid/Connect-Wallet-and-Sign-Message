93b4675312f9e1399b98cc4e916fa8a5e00ba8a8
Mian-Zaid <zaid7296@gmail.com>
2024-05-02 16:19:45 +0500

    sign message by providing message in format of SIWE

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,19 +3,60 @@ import { useUser, useAddress, useLogin } from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
 import { NextPage } from "next";
 import { useState } from "react";
+const { v4: uuidv4 } = require('uuid');
+
 
 
 const Home: NextPage = () => {
-  const { user } = useUser();
   const address = useAddress()
   const { login } = useLogin()
 
   const [signature, setSignature] = useState('N/A')
   const [addres, setAddres] = useState('N/A')
 
-  const message = 'Please sign me';
+  // const message = 'Please sign me';
+
+  const currentDate = new Date();
+  const expirationDate = new Date(currentDate.getTime() + 30 * 60000); // 30 minutes in milliseconds
+  const notBefore = new Date(currentDate.getTime() + 10 * 60000); // 10 minutes in milliseconds
+  const randomUUID = uuidv4();
+  console.log("UUID: ", randomUUID)
+
+  const loginOptions = {
+    version: "1", // The current version of the message, which MUST be 1 for this specification.
+    chainId: "1", // Chain ID to which the session is bound
+    nonce: randomUUID, // randomized token typically used to prevent replay attacks
+    "Issued At": currentDate.toISOString(),
+    "Expiration Time": expirationDate.toISOString(), // When this message expires
+    "Not Before": notBefore.toISOString(), // When this message becomes valid
+  };
 
   const sdk = useSDK();
+
+
+  /*{
+    
+    localhost:3000 wants you to sign in with your Ethereum account:
+    0x0319B28efEeF6131f1bcC833eCAA86E5c9c75867
+
+    Please ensure that the domain above matches the URL of the current website.
+
+    Version: 1
+    Chain ID: 1
+    Nonce: f280c5d0-cfbf-4f26-82ad-0bd8d11fe37c
+    Issued At: 2024-05-02T10:38:32.543Z
+    Expiration Time: 2024-05-02T10:59:24.973Z
+    Not Before: 2024-05-02T10:39:24.973Z
+
+  }*/
+
+  const objectToString = (obj: any) => {
+    return Object.entries(obj)
+      .map(([key, value]) => `${key}: ${value}`)
+      .join('\n');
+  }
+
+  const message = `localhost:3000 wants you to sign in with your Ethereum account:\n${address}\n\nPlease ensure that the domain above matches the URL of the current website.\n\n${objectToString(loginOptions)}`
 
   const signMessage = async () => {
     const sig = await sdk?.wallet?.sign(message)
@@ -57,13 +98,16 @@ const Home: NextPage = () => {
           Sign in with Etheruem
         </button>
 
-        {user && <p>You are Logged in as user: {user.address}</p>}
+        <br />
+        <br />
 
-        {/* <button onClick={signMessage}>Sign message</button>
+        <button onClick={signMessage}>Sign message</button>
         <p>Signature: {signature}</p>
         <br />
-        <button onClick={recoverAddress}>Recover Wallet Address</button>
-        <p>Wallet Address: {addres}</p> */}
+
+        <br />
+        {/* <button onClick={recoverAddress}>Recover Wallet Address</button>
+        <p>Recovered Wallet Address: {addres}</p> */}
       </div>
 
     </div>
